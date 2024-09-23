743dceb7e6a3e3628c7620130b0fc9bb96eb5ab6
Akshat Thakkar <akshatthakkar827@gmail.com>
2024-09-22 23:38:46 -0500

    Added overloaded methods for toPounds and toPoundsAndOunces (1 credit) and implemented toOunces function to convert pounds and ounces to total ounces (2 credits

--- src/main/java/com/csc/Converter.java
+++ src/main/java/com/csc/Converter.java
@@ -2,16 +2,32 @@ package com.csc;
 
 public class Converter {
 
-  public String toPounds(int ounces) {
-    double pounds = ounces / 16.0;
-    String unit = (pounds == 1.0) ? "lb" : "lbs";
-    return String.format("%.4f %s", pounds, unit);
-  }
+    // Overloaded method: Default to 0 ounces if no argument is passed
+    public String toPounds() {
+        return toPounds(0);  // Default to 0 ounces
+    }
 
-  public String toPoundsAndOunces(int ounces) {
-    int pounds = ounces / 16;
-    int remainingOunces = ounces % 16;
-    String poundUnit = (pounds == 1) ? "lb" : "lbs";
-    return pounds + " " + poundUnit + " " + remainingOunces + " oz";
-  }
+    public String toPounds(int ounces) {
+        double pounds = ounces / 16.0;
+        String unit = (pounds == 1.0) ? "lb" : "lbs";
+        return String.format("%.4f %s", pounds, unit);
+    }
+
+    // Overloaded method: Default to 0 ounces if no argument is passed
+    public String toPoundsAndOunces() {
+        return toPoundsAndOunces(0);  // Default to 0 ounces
+    }
+
+    public String toPoundsAndOunces(int ounces) {
+        int pounds = ounces / 16;
+        int remainingOunces = ounces % 16;
+        String poundUnit = (pounds == 1) ? "lb" : "lbs";
+        return pounds + " " + poundUnit + " " + remainingOunces + " oz";
+    }
+
+    // Convert from pounds and ounces to total ounces
+    public String toOunces(int pounds, int ounces) {
+        int totalOunces = (pounds * 16) + ounces;
+        return totalOunces + " ounces.";
+    }
 }
